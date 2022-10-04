dcdd7173244b80178ec492413d95ee5d09c91dbc
Gabriel Garcia <gabriel.garcia@estudiants.urv.cat>
2022-10-04 11:01:38 +0200

    Backend Done (State and Ops)

--- src/main/State.java
+++ src/main/State.java
@@ -1,10 +1,14 @@
 public class State {
     private final int pos_X;
     private final int pos_Y;
+    private final int height;
+    private final float time;  // Time to reach from the initial State
 
-    public State(int pos_X, int pos_Y) {
+    public State(int pos_X, int pos_Y, float time, int height) {
         this.pos_X = pos_X;
         this.pos_Y = pos_Y;
+        this.time = time;
+        this.height = height;
     }
 
 
@@ -12,25 +16,52 @@ public class State {
         return pos_X;
     }
 
-
     public int getPos_Y() {
         return pos_Y;
     }
 
+    public int getHeight() {
+        return height;
+    }
+
+    public float getTime() {
+        return time;
+    }
+
     /* Operations */
     public State up(int[][] map){
-        return null;
+        if( this.pos_Y-1 < 0 ||map[this.pos_X][this.pos_Y-1]==-1)return null;
+        int diferencia = map[this.pos_X][this.pos_Y+1]-this.height;
+        float time=0;
+        if(diferencia>=0) time+=diferencia;
+        else time+=0.5;
+        return new State(this.pos_X, this.pos_Y-1,time,map[this.pos_X][this.pos_Y-1]);
     }
 
     public State down(int[][] map){
-        return null;
+        if(this.pos_Y+1>=map[0].length || map[this.pos_X][this.pos_Y+1]==-1) return null;
+        int diferencia = map[this.pos_X][this.pos_Y+1]-this.height;
+        float time=0;
+        if(diferencia>=0) time+=diferencia;
+        else time+=0.5;
+        return new State(this.pos_X, this.pos_Y+1,time,map[this.pos_X][this.pos_Y+1]);
     }
 
     public State left(int[][] map){
-        return null;
+        if(this.pos_X-1<0 || map[this.pos_X-1][this.pos_Y]==-1) return null;
+        int diferencia = map[this.pos_X-1][this.pos_Y]-this.height;
+        float time=0;
+        if(diferencia>=0) time+=diferencia;
+        else time+=0.5;
+        return new State(this.pos_X, this.pos_Y+1,time,map[this.pos_X-1][this.pos_Y]);
     }
 
     public State right(int[][] map){
-        return null;
+        if(this.pos_X+1>=map.length || map[this.pos_X+1][this.pos_Y]==-1) return null;
+        int diferencia = map[this.pos_X+1][this.pos_Y]-this.height;
+        float time=0;
+        if(diferencia>=0) time+=diferencia;
+        else time+=0.5;
+        return new State(this.pos_X, this.pos_Y+1,time,map[this.pos_X+1][this.pos_Y]);
     }
 }
